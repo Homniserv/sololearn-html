342810011d9e697a332c93dbcaa146963f0626e1
moueza <221372+moueza@users.noreply.github.com>
2021-05-24 06:43:29 +0200

    Update geolocation.js

--- geolocation.js
+++ geolocation.js
@@ -1,25 +1,3 @@
-document.addEventListener('DOMContentLoaded',domloaded,false);
-//from https://github.com/moueza/rectangles-animation-javascript/blob/main/index.js
-function domloaded(){
-    const vers = " v";
-    console.log("in consol+" + vers);
-    // jQuery methods go here...
-    document.getElementById("geoloc").innerHTML =navigator.geolocation.getCurrentPosition(success, error, options);;
-
-  
-  
-    //canevas 3 :
-    var c3 = document.getElementById("myCanvas3");
-    const c33 = document.getElementById("myCanvas3");
-
-    var ctx3 = c3.getContext("2d");
-    const ctx33= c33.getContext("2d");
-
-    ctx3.moveTo(0, 0);
-    ctx3.lineTo(200, 100);
-    ctx3.stroke();
-}
-
 
 
 
@@ -43,4 +21,34 @@ function error(err) {
   console.warn(`ERROR(${err.code}): ${err.message}`);
 }
 
+
+
+
+
+
+
+
+
+
+document.addEventListener('DOMContentLoaded',domloaded,false);
+//from https://github.com/moueza/rectangles-animation-javascript/blob/main/index.js
+function domloaded(){
+    const vers = " v";
+    console.log("in consol+" + vers);
+    // jQuery methods go here...
+    document.getElementById("geoloc").innerHTML =navigator.geolocation.getCurrentPosition(success, error, options);
+
+  
+  
+    //canevas 3 :
+    var c3 = document.getElementById("myCanvas3");
+    const c33 = document.getElementById("myCanvas3");
+
+    var ctx3 = c3.getContext("2d");
+    const ctx33= c33.getContext("2d");
+
+    ctx3.moveTo(0, 0);
+    ctx3.lineTo(200, 100);
+    ctx3.stroke();
+}
 
