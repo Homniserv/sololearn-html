59c1992227c3da4673d9109de8bcebc71598e84b
moueza <221372+moueza@users.noreply.github.com>
2021-05-24 06:47:55 +0200

    Update geolocation.js

--- geolocation.js
+++ geolocation.js
@@ -44,8 +44,8 @@ function domloaded(){
     var c3 = document.getElementById("myCanvas3");
     const c33 = document.getElementById("myCanvas3");
 
-    var ctx3 = c3.getContext("2d");
-    const ctx33= c33.getContext("2d");
+   // var ctx3 = c3.getContext("2d");
+    //const ctx33= c33.getContext("2d");
 
     //ctx3.moveTo(0, 0);
     //ctx3.lineTo(200, 100);
